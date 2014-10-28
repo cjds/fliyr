2e95703145dd910f74739c46b3d1ad0fb255e929
cjds <cjds@live.com>
2014-10-28 19:38:25 -0400

    Hello:

--- app/views/form.blade.php
+++ app/views/form.blade.php
@@ -6,29 +6,37 @@
 	<title>Fliyr</title>
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
-	<link rel="shortcut icon" href="<?php echo asset('img/icon.ico');?>">
+	<link rel="shortcut icon" href="http://fliyr.com/img/icon.ico">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-	{{HTML::script('/js/foundation.min.js') }}
-	{{HTML::script('/js/jquery.form.min.js') }}
-	{{ HTML::style('css/foundation.css'); }}
+	<script src="http://fliyr.com/js/foundation.min.js"></script>
+
+	<script src="http://fliyr.com/js/jquery.form.min.js"></script>
+
+	<link media="all" type="text/css" rel="stylesheet" href="http://fliyr.com/css/foundation.css">
+
+
 
 	<style type="text/css">
+	input{
+		height: 30px;
+	}
 		::-webkit-input-placeholder{  
-   			font-family: 'Roboto', sans-serif;
+   			font-family: 'Oswald', sans-serif;
    			 padding-top: -3px;
 		}
 		:-moz-placeholder{
-			font-family: 'Roboto', sans-serif;
+			font-family: 'Oswald', sans-serif;
 			font-size: 1.2em;
 		} 
 
 		::-moz-placeholder{
-			font-family: 'Roboto', sans-serif;
+			font-family: 'Oswald', sans-serif;
 			font-size: 1.2em;
 		} 
 
 		:-ms-input-placeholder{
-			font-family: 'Roboto', sans-serif;
+			font-family: 'Oswald', sans-serif;
 			font-size: 1.2em;
 		}
 
@@ -43,11 +51,15 @@
 		   background: transparent;
 		   width: 285px;
 		   font-family: 'Oswald',sans-serif;
-		   color:#888; 
+		   color:#aaa; 
 		   line-height: 1;
 		   border: 1px solid #aaa;
 		   height: 30px;
 		   -webkit-appearance: none;
+   		}
+
+   		.styled-select select optgroup{
+   			font-weight: 300;
    		}
 
 		.styled-select {
@@ -88,9 +100,8 @@
 </head>
 <body>
 	<div class="magic" style="">
-	<div class="">
-		<div class="column large-2" >
-			<a href="<?php echo url('/');?>" style="">{{HTML::image('img/logo2.png', 'Fliyr',array('style'=>'width:123px;height:77px;margin:auto;margin-top:15px;display:block'));}}</a>
+		<div class="column large-2 small-12" >
+			<a href="http://fliyr.com" style=""><img src="http://fliyr.com/img/logo2.png" style="width:123px;height:77px;margin:auto;margin-top:15px;display:block" alt="Fliyr"></a>
 			
 			<p style="text-transform:uppercase;color:#969696;margin:auto;display:block;width:95px;font-weight:300"> Georgia Tech</p>
 		
@@ -99,26 +110,26 @@
 		</div>
 	
 	<div class="large-12 columns">
-	<a href="<?php echo url('/earlyaccess');?>" style="color:#58b946;font-weight:300;margin-left:auto;margin-right:auto;margin-top:2px;display:block;width:95px;text-transform:uppercase">Early Access</a>
-	<a href="<?php echo url('/aboutus');?>" style="color:#888;font-weight:300;margin-left:auto;margin-right:auto;margin-top:2px;display:block;width:95px;text-transform:uppercase">About Us</a>
+	<a href="http://fliyr.com/earlyaccess" style="color:#58b946;font-weight:300;margin-left:auto;margin-right:auto;margin-top:2px;display:block;width:95px;text-transform:uppercase">Early Access</a>
+	<a href="http://fliyr.com/aboutus" style="color:#888;font-weight:300;margin-left:auto;margin-right:auto;margin-top:2px;display:block;width:95px;text-transform:uppercase">About Us</a>
 	</div>
 	</div>
-	</div>
-	<div class="large-10 columns">
+
+	<div class="large-4 large-offset-0 small-12 columns">
 	<div class="row" style="margin-top:56px">
-		<div style="text-transform:uppercase;color:#58b946;">
-		Sign up for early access!{{HTML::image('img/icon.png', 'Fliyr',array('style'=>'display:inline-block;width:40px;margin-left:20px;height:auto'));}}
+		<div style="text-transform:uppercase;color:#58b946;font-size:1.2rem">
+		Sign up for early access!<img src="http://fliyr.com/img/icon.png" style="display:inline-block;width:40px;margin-left:20px;height:auto" alt="Fliyr">
 		</div>
 	</div>
 	<div class="row">	
 		<form action="https://docs.google.com/forms/d/1HXw--_sb8ybUlZ2Ucqo_L_Jm1Nh75oe4c4Yj2Sz9Dqo/formResponse" method="POST" id="ss-form" target="_self" onsubmit=""><ol role="list" class="ss-question-list" style="padding-left: 0">
 			</ol>
-
-		<input type="text" name="entry.1104918926" value="" class="ss-q-short" id="entry_1104918926" dir="auto" aria-label="First Name  " aria-required="true" placeholder="first name" required="" title="" style="margin-bottom:7px;width:150px">
-		<input type="text" name="entry.193253756" value="" class="ss-q-short" id="entry_193253756" dir="auto" aria-label="Last Name  " aria-required="true" required="" title="" placeholder="last name" style="margin-left:7px;width:190px">
+		
+		<input type="text" name="entry.1104918926" value="" class="ss-q-short" id="entry_1104918926" dir="auto" aria-label="First Name  " aria-required="true" placeholder="first name" required="" title="" style="margin-bottom:7px;width:190px"><br>
+		<input type="text" name="entry.193253756" value="" class="ss-q-short" id="entry_193253756" dir="auto" aria-label="Last Name  " aria-required="true" required="" title="" placeholder="last name" style="width:190px">
 <br>
-<div id="error" style="color:red;font-size:0.7em;display:none;">Please enter a Georgia Tech email</div>
-<input type="email" name="entry.868751442" value="" class="ss-q-short" id="entry_868751442" dir="auto" aria-label="GT E-mail address  Hey you have to enter a Georgia Tech E-mail address" aria-required="true" required="" title="Hey you have to enter a Georgia Tech E-mail ID" placeholder="GT email address" style="width:170px">
+<div id="error" style="margin-top:7px;color:red;font-size:0.7em;display:none;">Please enter a Georgia Tech email</div>
+<input type="email" name="entry.868751442" value="" class="ss-q-short" id="entry_868751442" dir="auto" aria-label="GT E-mail address  Hey you have to enter a Georgia Tech E-mail address" aria-required="true" required="" title="Hey you have to enter a Georgia Tech E-mail ID" placeholder="GT email address" style="margin-top:7px;width:190px">
 <div class='styled-select'>
 <select name="entry.421333035"  id="group_421333035_1" placeholder="please select your major">
 <option>-- please select your major --</option>
@@ -154,9 +165,9 @@
 <option>Materials Science and Engineering</option>
 <option>Mechanical Engineering</option>
 <option>Nuclear and Radiological Engineering</option>
-<option>Physics
-<option>Psychology
-<option>Public Policy
+<option>Physics</option>
+<option>Psychology</option>
+<option>Public Policy</option>
 </select>
 </div>
 
@@ -173,15 +184,167 @@
 </select>
 <br>
 </div>
-<p style="color:#999;margin-top:21px">Tell us what skills you have in your field of expertise</p>
-<input type="text" name="entry.1118930444" value="" id="entry_1118930444" dir="auto" aria-label="Tell us what skills you have in your field of expertise  " title="" style="margin-top:7px"><br>
-<input type="text" name="entry.1528480759" value="" id="entry_1528480759" dir="auto" aria-label="Tell us what skills you have in your field of expertise 2  " title="" style="margin-top:7px"><br>
-<input type="text" name="entry.1687596841" value="" id="entry_1687596841" dir="auto" aria-label="Tell us what skills you have in your field of expertise 3  " title="" style="margin-top:7px"><br>
+<p style="color:#888;margin-top:21px">Tell us what skills you have in your field of expertise</p>
+<div class='styled-select'>
+<select type="text" name="entry.1118930444" value="" id="entry_1118930444" dir="auto" aria-label="Tell us what skills you have in your field of expertise  " title="" style="">
+<optgroup>
+<option>-- please select a skill --</option>
+<option>Technical</option>
+<option>-- Front End Development</option>
+<option>-- Back End Development</option>
+<option>-- Application Development</option>
+<option>-- iOS Development</option>
+<option>-- Android Development</option>
+<option>-- Database Design</option>
+<option>-- Coding</option>
+<option>-- Information Architecture</option>
+<option>-- Data Mining</option>
+<option>-- Security</option>
+<option>-- S.E.O.</option>
+<option>Creative</option>
+<option>-- Graphic Design</option>
+<option>-- Photographer</option>
+<option>-- UX/UI</option>
+<option>-- General Design</option>
+<option>-- Industral Design</option>
+<option>-- Website Design</option>
+<option>-- Branding</option>
+<option>-- Ideation</option>
+<option>-- Content Management</option>
+<option>-- Copywriting</option>
+<option>-- Technical Writing</option>
+<option>-- Video Editing</option>
+<option>-- Advertising</option>
+<option>Business</option>
+<option>-- Networking</option>
+<option>-- Communication</option>
+<option>-- Leadership</option>
+<option>-- General Marketing</option>
+<option>-- Social Media Marketing</option>
+<option>-- Accounting</option>
+<option>-- Legal</option>
+<option>-- Communication</option>
+<option>-- Leadership</option>
+<option>-- Negotiation</option>
+<option>-- Business Development</option>
+<option>-- Project Management</option>
+<option>-- Fundraising</option>
+<option>-- Content Management</option>
+<option>-- Financial Management</option>
+<option>-- Multilingual</option>
+<option>-- Presenting</option>
+<option>-- Operations</option>
+<option>-- Customer Service</option>
+</optgroup>
+</select>
+</div>
 
-<p  style="color:#999;margin-top:21px">Is there anything else you could contribute to a startup?</p>
-<textarea style='width:350px;height:60px;margin-top:7px' name="entry.1201534306" class="ss-q-long" id="entry_1201534306"></textarea> 
+<div class='styled-select'>
+<select type="text" name="entry.1528480759" value="" id="entry_1528480759" dir="auto" aria-label="Tell us what skills you have in your field of expertise 2  " title="" >
+<option>-- please select a skill --</option>
+<option>Technical</option>
+<option>-- Front End Development</option>
+<option>-- Back End Development</option>
+<option>-- Application Development</option>
+<option>-- iOS Development</option>
+<option>-- Android Development</option>
+<option>-- Database Design</option>
+<option>-- Coding</option>
+<option>-- Information Architecture</option>
+<option>-- Data Mining</option>
+<option>-- Security</option>
+<option>-- S.E.O.</option>
+<option>Creative</option>
+<option>-- Graphic Design</option>
+<option>-- Photographer</option>
+<option>-- UX/UI</option>
+<option>-- General Design</option>
+<option>-- Industral Design</option>
+<option>-- Website Design</option>
+<option>-- Branding</option>
+<option>-- Ideation</option>
+<option>-- Content Management</option>
+<option>-- Copywriting</option>
+<option>-- Technical Writing</option>
+<option>-- Video Editing</option>
+<option>-- Advertising</option>
+<option>Business</option>
+<option>-- Networking</option>
+<option>-- Communication</option>
+<option>-- Leadership</option>
+<option>-- General Marketing</option>
+<option>-- Social Media Marketing</option>
+<option>-- Accounting</option>
+<option>-- Legal</option>
+<option>-- Communication</option>
+<option>-- Leadership</option>
+<option>-- Negotiation</option>
+<option>-- Business Development</option>
+<option>-- Project Management</option>
+<option>-- Fundraising</option>
+<option>-- Content Management</option>
+<option>-- Financial Management</option>
+<option>-- Multilingual</option>
+<option>-- Presenting</option>
+<option>-- Operations</option>
+<option>-- Customer Service</option>
+</select>
+</div>
+<div class='styled-select'>
+<select type="text" name="entry.1687596841" value="" id="entry_1687596841" dir="auto" aria-label="Tell us what skills you have in your field of expertise 3  " title="" >
+<option>-- please select a skill --</option>
+<option>Technical</option>
+<option>-- Front End Development</option>
+<option>-- Back End Development</option>
+<option>-- Application Development</option>
+<option>-- iOS Development</option>
+<option>-- Android Development</option>
+<option>-- Database Design</option>
+<option>-- Coding</option>
+<option>-- Information Architecture</option>
+<option>-- Data Mining</option>
+<option>-- Security</option>
+<option>-- S.E.O.</option>
+<option>Creative</option>
+<option>-- Graphic Design</option>
+<option>-- Photographer</option>
+<option>-- UX/UI</option>
+<option>-- General Design</option>
+<option>-- Industral Design</option>
+<option>-- Website Design</option>
+<option>-- Branding</option>
+<option>-- Ideation</option>
+<option>-- Content Management</option>
+<option>-- Copywriting</option>
+<option>-- Technical Writing</option>
+<option>-- Video Editing</option>
+<option>-- Advertising</option>
+<option>Business</option>
+<option>-- Networking</option>
+<option>-- Communication</option>
+<option>-- Leadership</option>
+<option>-- General Marketing</option>
+<option>-- Social Media Marketing</option>
+<option>-- Accounting</option>
+<option>-- Legal</option>
+<option>-- Communication</option>
+<option>-- Leadership</option>
+<option>-- Negotiation</option>
+<option>-- Business Development</option>
+<option>-- Project Management</option>
+<option>-- Fundraising</option>
+<option>-- Content Management</option>
+<option>-- Financial Management</option>
+<option>-- Multilingual</option>
+<option>-- Presenting</option>
+<option>-- Operations</option>
+<option>-- Customer Service</option>
+</select>
+</div>
+<p  style="color:#888;margin-top:21px">Is there anything else you could contribute to a startup?</p>
+<textarea style='width:330px;height:60px;margin-top:7px' name="entry.1201534306" class="ss-q-long" id="entry_1201534306"></textarea> 
 <br>
-<p  style="color:#999;margin-top:21px;display:inline-block">Do you have a startup idea of your own?</p>
+<p  style="color:#888;margin-top:21px;display:inline-block">Do you have a startup idea of your own?</p>
 <div class='styled-select' style="width:50px;display:inline-block;margin-bottom:-15px">
 <select name="entry.1832462633" value="Yes" id="group_1832462633_1" role="checkbox" style="width:50px;height:20px">
 <option>--</option>
@@ -191,7 +354,7 @@
 </select>
 </div>
 <br>
-<p  style="color:#999;margin-top:14px;display:inline-block">How did you hear about us?</p>
+<p  style="color:#888;margin-top:14px;display:inline-block">How did you hear about us?</p>
 <input style='margin-top:7px' name="entry.959435776" class="ss-q-long" id="entry_1201534306"/> 
 
 <input type="hidden" name="draftResponse" value="[,,&quot;-9084912540050282737&quot;]">
@@ -202,14 +365,17 @@
 <input type="submit" name="submit" value="SUBMIT" id="ss-submit" style="margin-top:21px;background:#58b946" class="jfk-button jfk-button-action button tiny ">
 </form>
 	</div>
+
 	</div>
+	<div class="column">
 	</div>
 
 <div id="five" style="display:none;background:#fff;width:100%;height:100%;background:rgba(0,0,0,0.3);z-index:1;position:absolute;color:#fff;font-size:40px;">
 <div style="color:#888;background:#fff;width:40%;height:40%;position:absolute;top:30%;left:30%;font-size:20px;padding:10px;margin:auto">
 <p style=";text-align:center;margin-top:21px">Thanks. Awesome dude. We'll contact you as soon as it is ready</p>
 <br>
-<a href="<?php echo url('/');?>" class="button" style="text-transform:uppercase;display:block;margin:auto;width:300px;margin-top:21px">Go Home, Bitch!</a>
+<a href="http://fliyr.com" class="button" style="text-transform:uppercase;display:block;margin:auto;width:300px;margin-top:21px">Go Home, Bitch!</a>
+</div>
 </div>
 </body>
 </html>
--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -2,6 +2,8 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
 	<title>Fliyr</title>
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <link href='http://fonts.googleapis.com/css?family=Nunito:700' rel='stylesheet' type='text/css'>
@@ -46,8 +48,24 @@
 		.black:hover{
 			background:#333;
 		}
-		p { background-color:white; filter:alpha(opacity=60); opacity:.6; }
+		
+		@media only screen and (min-width:63em){
+			.two{
+				margin-right: 10px;
+			
+			}
+		}
+		@media only screen and (min-width:63em){
+			.black{
+				margin-right: 50px;
+				float: right;
+			}
 
+			.two{
+				margin-right: 10px;
+				float: right;
+			}
+		}
 	</style>
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -63,7 +81,7 @@
         // wait for the DOM to be loaded 
         $(document).ready(function() { 
         	var person = prompt("Please Enter The Password", "");
-        	if(person!="Hummingbird92614"){
+        	if(person!=""){//Hummingbird92614
 				$('.magic').text("Sorry! You're not authorized. Wait a week for the prelaunch");
         	}
         	$('.magic').css("display",'block');
@@ -73,16 +91,17 @@
 </head>
 <body>
 <div class="column large-12 magic" style="display:none">
-	<div class="column large-4 large-offset-4" >
-			{{HTML::image('img/logo2.png', 'Fliyr',array('style'=>'width:200px;height:auto;margin-top:50px;margin-left:auto;margin-right: auto;display:block;'));}}
+	<div class="small-12 medium-12 column large-4 large-offset-4" >
+			{{HTML::image('img/logo2.png', 'Fliyr',array('style'=>'width:50%;height:auto;margin-top:50px;margin-left:auto;margin-right: auto;display:block;'));}}
 		<div class='row'>
-			<div class="column large-12" style="font-size:1.1em;text-align:center">
-				Uniting the expertise and inspiration of <br>the community to launch successful ideas
+			<div class="column small-12 medium-8 medium-offset-2 large-9 large-offset-2" >
+				<p style="text-align:center">Uniting the expertise and inspiration of
+				the community to launch successful ideas</p>
 			</div>
 			<div class="column large-12" style="font-size:1.1em">
 			<!--<a href="#" class="button tiny right black" style="background:#58b946;margin-right:13px;" >EARLY ACCESS</a></div>-->
-			<a href="<?php echo url('/earlyaccess');?>" class="button tiny right black" style="background:#58b946;margin-top:10px;margin-right:50px;text-transform:uppercase">georgia tech sign up</a>	
-				<a href="<?php echo url('/aboutus');?>" class="button tiny right black" style="margin-top:10px;margin-right:10px;text-transform:uppercase">learn more</a>	
+			<a href="<?php echo url('/earlyaccess');?>" class="button tiny black small-push-2 medium-push-4 large-push-0" style="background:#58b946;text-transform:uppercase;">georgia tech sign up</a>	
+				<a href="<?php echo url('/aboutus');?>" class="button tiny black two small-push-4 medium-push-4 large-push-0" style="text-transform:uppercase;">learn more</a>	
 
 			</div>
 		</div>
