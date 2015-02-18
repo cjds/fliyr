68f289e7c997e6bcd97139c647b38ce993953a8d
cjds <cjds@live.com>
2015-02-17 21:19:45 -0500

    Add heroku route DB

--- app/config/database.php
+++ app/config/database.php
@@ -1,4 +1,5 @@
 <?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 return array(
 
@@ -54,10 +55,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => isset($_SERVER['DB1_HOST']) ? $_SERVER['DB1_HOST'] : 'localhost',
-			'database'  => isset($_SERVER['DB1_NAME']) ? $_SERVER['DB1_NAME'] : 'fliyr',
-			'username'  => isset($_SERVER['DB1_USER']) ? $_SERVER['DB1_USER'] : 'root',
-			'password'  => isset($_SERVER['DB1_PASS']) ? $_SERVER['DB1_PASS'] : '',
+			'host'      => isset($url) ? $url['host'] :isset($_SERVER['DB1_HOST']) ? $_SERVER['DB1_HOST'] : 'localhost',
+			'database'  => isset($url) ? substr($url["path"], 1) :isset($_SERVER['DB1_NAME']) ? $_SERVER['DB1_NAME'] : 'fliyr',
+			'username'  => isset($url) ? $url['user'] :isset($_SERVER['DB1_USER']) ? $_SERVER['DB1_USER'] : 'root',
+			'password'  => isset($url) ? $url['pass'] :isset($_SERVER['DB1_PASS']) ? $_SERVER['DB1_PASS'] : '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
